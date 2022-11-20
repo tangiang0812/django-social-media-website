60e94b68207c2f1cb9dcbd364079a0562c753822
phl235363 <phl235363@gmail.com>
2022-11-21 03:41:41 +0700

    Add models profile , setting page , signup , sigin page ,

--- social_book/social_book/social_book/core/urls.py
+++ social_book/social_book/social_book/core/urls.py
@@ -2,5 +2,12 @@ from django.urls  import path
 from . import views
 
 urlpatterns=[
-    path('',views.index,name="index")
+    path('',views.index,name = 'index'),
+    path('settings',views.settings,name = 'settings'),
+    path ('signup',views.signup, name = 'signup'),
+    path ('signin',views.signin, name = 'signin'),
+    path ('logout',views.logout, name = 'logout'),
+
 ]
+
+
--- social_book/social_book/social_book/core/views.py
+++ social_book/social_book/social_book/core/views.py
@@ -1,7 +1,82 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from django.contrib.auth.models import User, auth
+from django.contrib.auth import authenticate
+from django.contrib import messages
 from django.http import HttpResponse
+from  django.contrib.auth.decorators import login_required
+from .models import Profile
 
 # Create your views here.
+@login_required(login_url='signin')
+
 def index(request):
     return render(request,'index.html')
+
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method=='POST':
+        if request.FILES.get('image') == None:
+            image=user_profile.profileimg
+            bio=request.POST['bio']
+            location=request.POST['location']
+    return render(request,'setting.html',{'user_profile': user_profile})
+
+
+def signup (request):
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        password2=request.POST['password2']
+        
+        if password==password2 :
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'Username Taken')
+                return redirect('signup')
+            else :
+                user = User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+
+                #Log user in and redirect to settings  page
+                user_login =auth.authenticate(username=username, password=password)
+                auth.login(request,user_login)
+                #create a Profile object for the  new user 
+                user_model=User.objects.get(username=username)
+                new_profile=Profile.objects.create(user=user_model,id_user=user_model.id)
+                new_profile.save()
+                return redirect ('signup')
+        else :
+            messages.info(request,'Password Not Matching')
+            return redirect('signup')
+           
+
+
+    else:
+        return render(request, 'signup.html' )
     
+
+def signin(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(username=username,password=password)
+        if  user is not None:
+            
+            auth.login(request,user)
+            return redirect('/')
+            
+        else :
+            print("FALSE")
+            messages.info(request,'Credentials Invalid')
+            return redirect('signin')
+    else:
+        return render(request,'signin.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
