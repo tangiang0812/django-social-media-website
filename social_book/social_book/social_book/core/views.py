0a345d586791a121a34eae88cb7d0cd6c833adf9
phl235363 <phl235363@gmail.com>
2022-11-21 05:17:23 +0700

    ADD upload funct

--- social_book/social_book/social_book/core/views.py
+++ social_book/social_book/social_book/core/views.py
@@ -4,23 +4,46 @@ from django.contrib.auth import authenticate
 from django.contrib import messages
 from django.http import HttpResponse
 from  django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile , Post
 
 # Create your views here.
+
 @login_required(login_url='signin')
-
 def index(request):
-    return render(request,'index.html')
+    user_object=User.objects.get(username=request.user.username)
+    user_profile=Profile.objects.get(user=user_object)
+    return render(request,'index.html',{'user_profile':user_profile})
 
+def upload(request):
+    return HttpResponse('<h1> Upload</h1>')
+
+@login_required(login_url='signin')
 def settings(request):
-    user_profile = Profile.objects.get(user=request.user)
-
-    if request.method=='POST':
+    user_profile=Profile.objects.get(user=request.user) 
+    print (user_profile)
+    if request.method == 'POST':    
         if request.FILES.get('image') == None:
-            image=user_profile.profileimg
-            bio=request.POST['bio']
-            location=request.POST['location']
-    return render(request,'setting.html',{'user_profile': user_profile})
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile': user_profile})
+
 
 
 def signup (request):
@@ -76,7 +99,7 @@ def signin(request):
     else:
         return render(request,'signin.html')
 
-
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
